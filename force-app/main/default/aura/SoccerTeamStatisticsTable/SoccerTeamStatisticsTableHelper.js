9caa9c3a41e9845c291b1446710a970fe98433d7
KovalchikAT <KovalchikAT@i.ua>
2021-11-05 17:00:31 +0200

    last home work commit

--- force-app/main/default/aura/SoccerTeamStatisticsTable/SoccerTeamStatisticsTableHelper.js
+++ force-app/main/default/aura/SoccerTeamStatisticsTable/SoccerTeamStatisticsTableHelper.js
@@ -6,7 +6,22 @@
             "limitNumber": component.get("v.limitNumber")
         });
         action.setCallback(this, function(response) {
+            if (response.getState==="SUCCESS"){
             component.set("v.teams", response.getReturnValue());
+            }else
+            if (response.getState==="ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                 errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+            
+
         });
         
         $A.enqueueAction(action);
